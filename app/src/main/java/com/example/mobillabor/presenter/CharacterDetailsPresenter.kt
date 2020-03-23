2f3aa617ba14a98bb007b2aa5bbc8c793ca2befe
Adam Dobo <doboadam19@gmail.com>
2020-03-23 17:42:16 +0100

    Add presenters

---
+++ app/src/main/java/com/example/mobillabor/presenter/CharacterDetailsPresenter.kt
@@ -0,0 +1,5 @@
+package com.example.mobillabor.presenter
+
+import com.example.mobillabor.view.details.CharacterDetailsScreen
+
+class CharacterDetailsPresenter : BasePresenter<CharacterDetailsScreen>()
---
+++ app/src/main/java/com/example/mobillabor/presenter/CharacterListPresenter.kt
@@ -0,0 +1,6 @@
+package com.example.mobillabor.presenter
+
+import com.example.mobillabor.view.list.CharacterListScreen
+
+class CharacterListPresenter: BasePresenter<CharacterListScreen>() {
+}
